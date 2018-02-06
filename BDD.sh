cc411d34b857d86d23ab1f927698dcd8cface076
duttadeep55 <duttadeep55@outlook.com>
2018-02-06 22:23:37 +0530

    properties.json updated with new server ip

--- BDD.sh
+++ BDD.sh
@@ -1,4 +1,4 @@
-output=$(curl http://demo-ecs-dev-elb-257222220.us-west-2.elb.amazonaws.com/)
+output=$(curl http://demo-ECS-DEV-ELB-1035214322.us-west-2.elb.amazonaws.com/)
 flag="Failed"
 if [[ $output == "Hello, HUDSON BAY !!" ]]
 then
