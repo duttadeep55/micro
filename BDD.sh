6822f34e2d7f0e601ac3d2a5b1c8cefbd37bdb0b
duttadeep55 <duttadeep55@outlook.com>
2018-02-06 19:31:41 +0530

    BDD included

--- BDD.sh
+++ BDD.sh
@@ -1,4 +1,4 @@
-output=$(curl http://localhost:8080)
+output=$(curl http://demo-ecs-dev-elb-257222220.us-west-2.elb.amazonaws.com/)
 flag="Failed"
 if [[ $output == "Greetings from Kloud9!" ]]
 then
