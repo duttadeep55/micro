8130063043f9e6de6fb6740a581697b2adb52b0f
Deep Dutta <35916964+vajra55@users.noreply.github.com>
2018-05-15 22:21:12 +0530

    Update BDD.sh

--- BDD.sh
+++ BDD.sh
@@ -1,4 +1,4 @@
-output=$(curl http://demo-ECS-DEV-ELB-1035214322.us-west-2.elb.amazonaws.com/)
+output=$(curl http://demo-ECS-DEV-ELB-741511207.us-west-2.elb.amazonaws.com/)
 flag="Failed"
 if [[ $output == "Hello, HUDSON BAY !!" ]]
 then
