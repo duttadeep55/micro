adeca5af65e2183a030a125382cc8c2ca7c3ca00
Deep Dutta <duttadeep55@outlook.com>
2019-08-17 14:32:22 +0530

    Delete BDD.sh

--- BDD.sh
+++
@@ -1,9 +0,0 @@
-output=$(curl http://demo-ECS-DEV-ELB-741511207.us-west-2.elb.amazonaws.com/)
-flag="Failed"
-if [[ $output == "Hello, HUDSON BAY !!" ]]
-then
- flag="Passed"
-else
- flag="Failed"
-fi 
-echo $flag
